2f5e3a7300e54781b25b5ae0c6b7160f74612db4
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-04-18 16:20:45 -0300

    Update script5.js

--- Meses do ano/script5.js
+++ Meses do ano/script5.js
@@ -1,3 +1,4 @@
+/*if ternário*/
 var numero = parseInt(prompt("Digite um número de 1 a 12"));
 var mesesDoAno = numero == 1? "Janeiro" : numero == 2? "Fevereiro" : numero == 3? "Março" : numero == 4? "Abril" : numero == 5? "Maio" : numero == 6? "Junho" : numero == 7? "Julho" : numero == 8? "Agosto" : numero == 9? "Setembro" : numero == 10? "Outubro" : numero == 11? "Novembro" : numero == 12? "Dezembro" : "Mês inválido";
 alert(mesesDoAno);
