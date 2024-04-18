c7465992d4ec5a142f58ade3c842e4b6f0847bb4
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-04-18 16:51:00 -0300

    Add files via upload

---
+++ área do retangulo/script.js
@@ -0,0 +1,10 @@
+let base;
+let altura;
+let area;
+
+base = parseInt(prompt("Digite o valor da base"));
+altura = parseInt(prompt("Digite o valor da altura"));
+
+area = base * altura;
+
+alert(`Com a base ${base}cm e a altura ${altura}cm, a área será ${area}`)
