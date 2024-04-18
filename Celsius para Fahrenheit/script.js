1b1e8f9ec6d338c66fd3fed4969836642d4b2480
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-04-18 17:04:11 -0300

    Add files via upload

---
+++ Celsius para Fahrenheit/script.js
@@ -0,0 +1,7 @@
+// converte graus fahrenheitpara celsius
+let celsius
+
+let fahrenheit = parseFloat(prompt("Digite o valor de graus Fahrenheit"));
+celsius = 5 * (fahrenheit - 32) / 9;
+
+alert(`${fahrenheit}°F em celsius será ${celsius}°C`);
