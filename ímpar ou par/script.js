90511ee45da2613ba4621999cd5c75a6ced92511
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-04-18 19:47:55 -0300

    Add files via upload

---
+++ ímpar ou par/script.js
@@ -0,0 +1,9 @@
+// Determina se o valor digitado é ímpar ou par
+
+let numero = parseInt(prompt("Digite um número qualquer"));
+
+if (numero % 2 == 0){
+alert("Esse valor é par");
+} else{
+    alert("Esse número é ímpar");
+}
