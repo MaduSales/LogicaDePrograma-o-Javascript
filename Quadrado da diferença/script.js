4e820926be51dd622b233348912bdf08baaabb4b
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-04-18 21:48:31 -0300

    Add files via upload

---
+++ Quadrado da diferença/script.js
@@ -0,0 +1,8 @@
+// Imprimirá o resultado  do quadrado da diferença  do primeiro valor pelo segundo
+
+let resultado
+let A = parseInt(prompt("Escreva o valor de A"));
+let B = parseInt(prompt("Escreva o valor de B"));
+
+resultado = (A - B) ** 2;
+alert("O valor da conta é "+ resultado);
