25ac758edb42859887ca53c194d0bf0065b81743
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-04-18 21:54:01 -0300

    Add files via upload

---
+++ Quadrado da soma/script.js
@@ -0,0 +1,8 @@
+// calcula o quadrado da soma de três valores
+let resultado
+let A = parseInt(prompt("Escreva o valor de A"));
+let B = parseInt(prompt("Escreva o valor de B"));
+let C = parseInt(prompt("Escreva o valor de C"));
+
+resultado = (A + B + C) ** 2
+alert("O valor do quadrado da soma é "+ resultado);
