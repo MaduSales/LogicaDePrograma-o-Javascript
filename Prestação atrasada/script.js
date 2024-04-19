7b0a26cd0d32592c248e4143fb984ce8a5a1b048
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-04-18 21:37:48 -0300

    Add files via upload

---
+++ Prestação atrasada/script.js
@@ -0,0 +1,11 @@
+// Efetuar o cálculo e apresentar o valor de uma prestação em atraso
+
+let prestacao
+let valor = parseFloat(prompt("Escreva o valor inicial da prestação"));
+let taxa = parseFloat(prompt("Escreva o valor da taxa de juros com números inteiros"));
+let tempo = parseFloat(prompt("Escreva em quantos meses está parcelado"));
+
+prestacao = valor + (valor * taxa/100) * tempo
+alert("O valor da prestação atualmente é de "+ prestacao +" reais");
+
+
