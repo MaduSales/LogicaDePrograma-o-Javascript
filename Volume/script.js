1f9f03e7893b8638897bc4fe3e8c65cd0ba5bc55
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-04-20 20:10:35 -0300

    Add files via upload

---
+++ Volume/script.js
@@ -0,0 +1,9 @@
+// Calcular e apresentar o volume de uma lata de óleo
+
+let volume;
+let R = parseFloat(prompt("Escreva um valor do raio da lata"));
+let altura = parseFloat(prompt("Escreva um valor da altura da lata"));
+
+volume = 3.14 * R^2 * altura
+alert("volume da lata de óleo é de " + volume +" litros");
+
