6c83a2af84f825259f99fcfc54734191be9fc158
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-04-20 20:06:58 -0300

    Add files via upload

---
+++ Verificacao do numero 3/script.js
@@ -0,0 +1,10 @@
+// Só valida números menores ou iguais a 3
+
+let numero = parseInt(prompt("Digite um valor inteiro"));
+
+if (numero == 3) {
+    alert("Este número é válido pois é "+ numero)
+} else{
+    alert("Este número não é válido")
+}
+
